b7e65f7fda659806432a11a19e5da95bdbe1e3c6
Daniel-Gomes <danielgomes@ioasys.com.br>
2021-09-26 19:19:58 -0300

    Swiftgen added

--- Books/Common/Package.swift
+++ Books/Common/Package.swift
@@ -20,7 +20,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Common",
-            dependencies: []),
+            dependencies: [],
+            resources:
+                [
+                    .copy("Resources/Fonts"),
+                    .copy("Resources/Colors"),
+                    .copy("Resources/Localizable.strings")
+                ]),
         .testTarget(
             name: "CommonTests",
             dependencies: ["Common"]),
